deffedad4b87ba85d2f38e632270468e992b0dfa
Tomasz Pęczek <tpeczek@gmail.com>
2019-12-22 21:43:07 +0100

    Dropping support for v1.x.y and moving v2.x.y to maintenance (#22)

--- Lib.AspNetCore.Mvc.JqGrid.Core/Helpers/CompatibilityHelper.cs
+++ Lib.AspNetCore.Mvc.JqGrid.Core/Helpers/CompatibilityHelper.cs
@@ -9,13 +9,7 @@ namespace Lib.AspNetCore.Mvc.JqGrid.Core.Helpers
         {
             get
             {
-#if ASPNETCORE10 && !NETSTANDARD2_0
-                return TaskCache.CompletedTask;
-#elif NETSTANDARD2_0
                 return Task.CompletedTask;
-#else
-            // Not implemented, compiler break
-#endif
             }
         }
     }
